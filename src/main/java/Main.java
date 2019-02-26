45c1662d0a2bd4e82d3a41ae808542d561715211
quanticc <quanticc@users.noreply.github.com>
2019-02-26 02:46:54 -0300

    Split repo into BOM and no BOM modules

--- src/main/java/Main.java
+++
@@ -1,8 +0,0 @@
-import discord4j.core.DiscordClientBuilder;
-
-public class Main {
-
-    public static void main(String[] args) {
-        new DiscordClientBuilder(System.getenv("token")).build().login().block();
-    }
-}
