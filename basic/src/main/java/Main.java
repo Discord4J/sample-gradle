46e5a4c847e5373ebefc1e3c0257d79448cbd0a8
quanticc <quanticc@users.noreply.github.com>
2019-03-22 11:48:00 -0300

    Upgrade to Discord4J 3.0.1

--- basic/src/main/java/Main.java
+++ basic/src/main/java/Main.java
@@ -1,11 +1,13 @@
 import discord4j.core.DiscordClient;
 import discord4j.core.DiscordClientBuilder;
+import discord4j.core.event.domain.guild.GuildCreateEvent;
 import discord4j.core.event.domain.lifecycle.ReadyEvent;
 import discord4j.core.event.domain.message.MessageCreateEvent;
 import discord4j.core.object.entity.Message;
 import discord4j.core.object.entity.User;
-import discord4j.store.api.readonly.ReadOnlyStoreService;
-import discord4j.store.jdk.JdkStoreService;
+import discord4j.core.shard.ShardingClientBuilder;
+import reactor.core.publisher.Flux;
+import reactor.core.scheduler.Schedulers;
 
 public class Main {
 
