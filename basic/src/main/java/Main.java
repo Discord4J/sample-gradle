d02da2c9a3201628434bdbb5ca4bc5e6d729f77a
quanticc <quanticc@users.noreply.github.com>
2019-12-25 13:11:52 -0300

    Update gradle sample project

--- basic/src/main/java/Main.java
+++ basic/src/main/java/Main.java
@@ -4,17 +4,47 @@ import discord4j.core.event.domain.lifecycle.ReadyEvent;
 import discord4j.core.event.domain.message.MessageCreateEvent;
 import discord4j.core.object.entity.Message;
 import discord4j.core.object.entity.User;
+import reactor.core.publisher.Mono;
 
 public class Main {
 
     public static void main(String[] args) {
-        DiscordClient client = new DiscordClientBuilder(System.getenv("token")).build();
+        /*        DiscordClient discord = DiscordClient.create(System.getenv("token"));
+
+         *//*        GatewayDiscordClient client = discord.login().block();*/
+
+        DiscordClient client = new DiscordClientBuilder(System.getenv("token"))
+                .build();
+
+        // option 1
+        client.getEventDispatcher().on(MessageCreateEvent.class)
+                .flatMap(event ->
+                        Mono.fromRunnable(() -> {
+                            // call listener.onMessage(event)
+                        }).onErrorResume(t -> {
+                            // log
+                            return Mono.empty();
+                        }))
+                .subscribe();
+
+        // option 2
+//        client.getEventDispatcher().on(MessageCreateEvent.class)
+//                .flatMap(event ->
+//                        listener.onMessage(event) // make it return Mono.empty() for now
+//                        .onErrorResume(t -> {
+//                            // log
+//                            return Mono.empty();
+//                        }))
+//                .subscribe();
 
         client.getEventDispatcher().on(ReadyEvent.class)
-                .subscribe(event -> {
+                .flatMap(event -> {
                     User self = event.getSelf();
-                    System.out.println(String.format("Logged in as %s#%s", self.getUsername(), self.getDiscriminator()));
-                });
+                    System.out.println(String.format("Logged in as %s#%s", self.getUsername(),
+                            self.getDiscriminator()));
+                    return Mono.empty();
+                })
+                .subscribe();
 
         client.getEventDispatcher().on(MessageCreateEvent.class)
                 .map(MessageCreateEvent::getMessage)
